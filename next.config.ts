06bb2ed093dbc64ee0f2a48d41a199812326ec98
Rigsby001 <ignaciorafaelgs4@gmail.com>
2025-10-20 19:09:40 -0400

    feat: ajustes

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   basePath: "/portfolio",
   output: "export",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,22 +54,7 @@ const experience:  {
 export default function Home() {
   return (
     <>
-    
-    <HeaderComponent></HeaderComponent>
-    
-    <AboutMeSection name="Texting" imageAvatarUrl="/img/imagen_cv.jpg" description=" Ingeniero en Computación con sólida formación académica y experiencia en el diseño, desarrollo y optimización de
- soluciones tecnológicas. Actualmente me desempeño como Desarrollador Full Stack, integrando conocimientos de
- frontend, backend y bases de datos para construir aplicaciones web robustas y eficientes. Destaco por mi capacidad para
- abordar problemas complejos con soluciones escalables y alineadas a buenas prácticas de ingeniería de software"></AboutMeSection>
-    
-    
-  <SkillsSection></SkillsSection>
-
-
-  <ProjectsComponent projects={projects} nameTitle="Proyectos"></ProjectsComponent>
-  
-  <ExperienceComponent experience={experience} titleSection="Experiencia" ></ExperienceComponent>
-  <ContactComponent></ContactComponent>  
+  <h1>HOLA DEPLOY </h1>
   </>
   );
 }
